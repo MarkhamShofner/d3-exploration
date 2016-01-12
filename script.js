61bda2451135ab1998eb9a96565f390c3c024268
Markham Shofner <markham.shofner@gmail.com>
2016-01-12 15:12:57 -0500

    map indicators from 2010 against their counterparts from 2011. create dynamic range and domain extent functionality

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-
 // var scale = d3.scale.linear()
 //   .domain([0, 1])
 //   .range([0, 100]);
@@ -7,27 +6,26 @@ var svg = d3.select("body").append("svg")
   .attr("width", 350)
   .attr("height", 350);
 
-var rect = svg.append("rect")
-  .attr("width", 20)
-  .attr("height", 20);
+var xScale = d3.scale.linear().range([0, 10000]);
+var yScale = d3.scale.linear().range([0, 10000]);
 
 function render(data) {
+  xScale.domain(d3.extent(data, function (d){ return d["2010 [YR2010]"]; }));
+  yScale.domain(d3.extent(data, function (d){ return d["2011 [YR2011]"]; }));
+
   // Bind data
   var circles = svg.selectAll("circle").data(data);
-
   // Enter
   circles.enter().append("circle")
     .attr("r", 3);
-
   // Update
   circles
     .attr("cx", function(d) {
-      return d["2010 [YR2010]"];
+      return yScale(d["2010 [YR2010]"]);
     })
-    .attr("cy", 50);
-    // .attr("cy", function(d) {
-    //   return d.y;
-    // });
+    .attr("cy", function(d) {
+      return yScale(d["2011 [YR2011]"]);
+    });
 
   // Exit
   circles.exit().remove();
@@ -35,24 +33,17 @@ function render(data) {
 
 
 
-// "Data Source","World Development Indicators",
-// "Last Updated Date","2015-12-22",
+// Data from database: World Development Indicators
+// Last Updated: 12/22/2015
 
-d3.csv("./raw_data/Popular_indicators_Data.csv", type, function(myArrayOfObjects) {
-  // myArrayOfObjects.forEach(function(d) {
-  //   if (d["Indicator Name"] === "Population, ages 0-14 (% of total)") {
-  //     console.log(d["Country Name"] + " " + d["Indicator Name"] + ": " + d["2010"]);
-  //   }
-  // });
+d3.csv("./raw_data/Popular_indicators_Data.csv", type, function(data) {
 
-  // var min = d3.min(myArrayOfObjects, function (d) {if()})
-
-  render(myArrayOfObjects);
+  render(data);
 });
 
 function type(d) {
   // d["Indicator Name"] = parseFloat(d["Indicator Name"]);
-  d["2010"] = parseFloat(d["2010"]);
+  // d["2010"] = parseFloat(d["2010"]);
   // console.log(d["2010"]);
   return d;
 }
