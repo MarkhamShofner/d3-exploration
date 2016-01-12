f7cb8c349b70b6b6f918b73306f5657d01b0873a
Markham Shofner <markham.shofner@gmail.com>
2016-01-12 17:36:56 -0500

    add in new styles for data representation. change size of circle and color of circle based on fields

--- script.js
+++ script.js
@@ -1,12 +1,16 @@
-// var scale = d3.scale.linear()
-//   .domain([0, 1])
-//   .range([0, 100]);
 
-var outerWidth = 350;
-var outerHeight = 350;
+var outerWidth = 500;
+var outerHeight = 500;
 var circleRadius = 2;
 var xColumn = "2010 [YR2010]";
 var yColumn = "2011 [YR2011]";
+var rColumn = "2012 [YR2012]";
+var colorColumn = "Series Name";
+var filter1 = "GDP growth (annual %)";
+
+// tethering size of the circle to another variable
+var rMin = 2;
+var rMax = 7;
 
 var svg = d3.select("body").append("svg")
   .attr("width", outerWidth)
@@ -14,24 +18,41 @@ var svg = d3.select("body").append("svg")
 
 var xScale = d3.scale.linear().range([0, outerWidth]);
 var yScale = d3.scale.linear().range([outerHeight, 0]);
+var rScale = d3.scale.linear().range([rMin, rMax]);
 
 function render(data) {
-  xScale.domain(d3.extent(data, function (d){ return d[xColumn]; }));
-  yScale.domain(d3.extent(data, function (d){ return d[yColumn]; }));
+  xScale.domain(d3.extent(data, function(d) {
+    return d[xColumn];
+  }));
+  yScale.domain(d3.extent(data, function(d) {
+    return d[yColumn];
+  }));
+  rScale.domain(d3.extent(data, function(d) {
+    return d[rColumn];
+  }));
+  var colorScale = d3.scale.category20();
 
   // Bind data
   var circles = svg.selectAll("circle").data(data);
   // Enter
-  circles.enter().append("circle")
-    .attr("r", 3);
+  circles.enter().append("circle");
+  // .attr("r", 3);
+
   // Update
   circles
     .attr("cx", function(d) {
-      return xScale(d["2010 [YR2010]"]);
+      return xScale(d[xColumn]);
     })
     .attr("cy", function(d) {
-      return yScale(d["2011 [YR2011]"]);
+      return yScale(d[yColumn]);
+    })
+    .attr("r", function(d) {
+      return rScale(d[rColumn]);
+    })
+    .attr("fill", function(d) {
+      return colorScale(d[colorColumn]);
     });
+
 
   // Exit
   circles.exit().remove();
@@ -47,8 +68,8 @@ d3.csv("./raw_data/Popular_indicators_Data.csv", type, function(data) {
 
 function type(d) {
   // d["Indicator Name"] = parseFloat(d["Indicator Name"]);
-  d["2010 [YR2010]"] = parseFloat(d["2010 [YR2010]"]);
-  d["2011 [YR2011]"] = parseFloat(d["2011 [YR2011]"]);
-  // console.log(d["2010"]);
+  d[xColumn] = parseFloat(d[xColumn]);
+  d[yColumn] = parseFloat(d[yColumn]);
+  d[rColumn] = parseFloat(d[rColumn]);
   return d;
 }
