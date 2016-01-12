1a222f5d0dfb2638f184069dc377e4e0910a4357
Markham Shofner <markham.shofner@gmail.com>
2016-01-12 14:28:23 -0500

    incorporate new, more robust, data set for popular country health indicators. move old education files out.

--- script.js
+++ script.js
@@ -17,12 +17,12 @@ function render(data) {
 
   // Enter
   circles.enter().append("circle")
-    .attr("r", 10);
+    .attr("r", 3);
 
   // Update
   circles
     .attr("cx", function(d) {
-      return d["2015"];
+      return d["2010 [YR2010]"];
     })
     .attr("cy", 50);
     // .attr("cy", function(d) {
@@ -38,13 +38,15 @@ function render(data) {
 // "Data Source","World Development Indicators",
 // "Last Updated Date","2015-12-22",
 
-d3.csv("./raw_data/12-22-2015_data.csv", type, function(myArrayOfObjects) {
-  myArrayOfObjects.forEach(function(d) {
-    if (d["Indicator Name"] === "Population, ages 0-14 (% of total)") {
-      console.log(d["Country Name"] + " " + d["Indicator Name"] + ": " + d["2010"]);
-      render(d["2010"]);
-    }
-  });
+d3.csv("./raw_data/Popular_indicators_Data.csv", type, function(myArrayOfObjects) {
+  // myArrayOfObjects.forEach(function(d) {
+  //   if (d["Indicator Name"] === "Population, ages 0-14 (% of total)") {
+  //     console.log(d["Country Name"] + " " + d["Indicator Name"] + ": " + d["2010"]);
+  //   }
+  // });
+
+  // var min = d3.min(myArrayOfObjects, function (d) {if()})
+
   render(myArrayOfObjects);
 });
 
