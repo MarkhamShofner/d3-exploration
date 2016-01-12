fa5fbd95a58d930e5388ee3c338a877db9c40216
Markham Shofner <markham.shofner@gmail.com>
2016-01-11 23:06:37 -0500

    flesh out base structure for dom manipulation

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-console.log("hello world");
+// console.log("hello world");
+//
+// var svg = d3.select("body").append("svg")
+//   .attr("width", 400)
+//   .attr("height", 400);
+//
+// var g = svg.append("g");
