96686e62f627dce522f29693086e454608847e84
Markham Shofner <markham.shofner@gmail.com>
2016-01-12 13:36:14 -0500

    render visual representation of csv data file on the page

--- script.js
+++ script.js
@@ -1,26 +1,51 @@
-//
-//
-// var myArrayOfObjects = [
-//   { x: 100, y: 100},
-//   { x: 130, y: 120},
-//   { x: 80 , y: 180},
-//   { x: 180, y: 80 },
-//   { x: 180, y: 40 }
-// ];
-//
-// myArrayOfObjects.forEach(function (d){
-//   console.log(d.x + ", " + d.y);
-// });
+
+// var scale = d3.scale.linear()
+//   .domain([0, 1])
+//   .range([0, 100]);
+
+var svg = d3.select("body").append("svg")
+  .attr("width", 350)
+  .attr("height", 350);
+
+var rect = svg.append("rect")
+  .attr("width", 20)
+  .attr("height", 20);
+
+function render(data) {
+  // Bind data
+  var circles = svg.selectAll("circle").data(data);
+
+  // Enter
+  circles.enter().append("circle")
+    .attr("r", 10);
+
+  // Update
+  circles
+    .attr("cx", function(d) {
+      return d["2015"];
+    })
+    .attr("cy", 50);
+    // .attr("cy", function(d) {
+    //   return d.y;
+    // });
+
+  // Exit
+  circles.exit().remove();
+}
+
 
 
 // "Data Source","World Development Indicators",
 // "Last Updated Date","2015-12-22",
+
 d3.csv("./raw_data/12-22-2015_data.csv", type, function(myArrayOfObjects) {
   myArrayOfObjects.forEach(function(d) {
     if (d["Indicator Name"] === "Population, ages 0-14 (% of total)") {
       console.log(d["Country Name"] + " " + d["Indicator Name"] + ": " + d["2010"]);
+      render(d["2010"]);
     }
   });
+  render(myArrayOfObjects);
 });
 
 function type(d) {
