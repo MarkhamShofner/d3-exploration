0ce09943ce7ec7a750078e8db513d826713379f9
Markham Shofner <markham.shofner@gmail.com>
2016-01-12 11:00:03 -0500

    create test data file, sucessfully extract data points from specified fields

--- script.js
+++ script.js
@@ -1,7 +1,29 @@
-// console.log("hello world");
 //
-// var svg = d3.select("body").append("svg")
-//   .attr("width", 400)
-//   .attr("height", 400);
 //
-// var g = svg.append("g");
+// var myArrayOfObjects = [
+//   { x: 100, y: 100},
+//   { x: 130, y: 120},
+//   { x: 80 , y: 180},
+//   { x: 180, y: 80 },
+//   { x: 180, y: 40 }
+// ];
+//
+// myArrayOfObjects.forEach(function (d){
+//   console.log(d.x + ", " + d.y);
+// });
+
+
+// "Data Source","World Development Indicators",
+// "Last Updated Date","2015-12-22",
+d3.csv("./raw_data/test_Aruba.csv", type, function (myArrayOfObjects){
+  myArrayOfObjects.forEach(function (d){
+    console.log(d["Country Name"] + " " + d["Indicator Name"] + ": " + d["2010"]);
+  });
+});
+
+function type(d){
+  // d["Indicator Name"] = parseFloat(d["Indicator Name"]);
+  d["2010"] = parseFloat(d["2010"]);
+  // console.log(d["2010"]);
+  return d;
+}
