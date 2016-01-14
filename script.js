34aedd80af08059ada4ab025cd4887f94620eae4
Markham Shofner <markham.shofner@gmail.com>
2016-01-14 12:26:40 -0500

    add in html filter for 21 data options. listen to first option and assign as filter option

--- script.js
+++ script.js
@@ -1,15 +1,20 @@
 // base size/positioning variables
 var outerWidth = 500;
 var outerHeight = 500;
-var margin = { left: 90, top: 30, right: 90, bottom: 30 };
+var margin = {
+  left: 90,
+  top: 30,
+  right: 90,
+  bottom: 30
+};
 var circleRadius = 2;
 
 // tethering size of the circle to another variable
 var rMin = 2;
 var rMax = 7;
 
-var innerWidth  = outerWidth  - margin.left - margin.right;
-var innerHeight = outerHeight - margin.top  - margin.bottom;
+var innerWidth = outerWidth - margin.left - margin.right;
+var innerHeight = outerHeight - margin.top - margin.bottom;
 
 // dataset columns
 var xColumn = "2010 [YR2010]";
@@ -20,8 +25,18 @@ var colorColumn = "Series Name";
 // filtered rows
 var filter2 = "GDP growth (annual %)";
 var filter1 = "GNI per capita, PPP (current international $)";
-var filter3 = "Foreign direct investment, net (BoP, current US$)";
-var filter2 = "Internet users (per 100 people)";
+var filter2 = "Foreign direct investment, net (BoP, current US$)";
+
+var filter1 = function getField() {
+  console.log("filter1");
+  return d3.select("select").node().value;
+}();
+
+d3.select("select").on("change",function(){
+  var filter1 = this.getField();
+});
+
+
 
 // create svg element based on set variable measures
 var svg = d3.select("body").append("svg")
@@ -41,9 +56,9 @@ var rScale = d3.scale.linear().range([rMin, rMax]);
 
 // set axes
 var xAxis = d3.svg.axis().scale(xScale).orient("bottom")
-  .ticks(5);                   // Use approximately 5 ticks marks.
-  // .tickFormat(d3.format("s")); // Use intelligent abbreviations, e.g. 5M for 5 Million
-  // .outerTickSize(0);          // Turn off the marks at the end of the axis.
+  .ticks(5); // Use approximately 5 ticks marks.
+// .tickFormat(d3.format("s")); // Use intelligent abbreviations, e.g. 5M for 5 Million
+// .outerTickSize(0);          // Turn off the marks at the end of the axis.
 var yAxis = d3.svg.axis().scale(yScale).orient("left");
 
 //render data
@@ -78,7 +93,7 @@ function render(dataS, data1, data2) {
     })
     .attr("cy",
       function(d) {
-        return yScale(d["second_filter"]);
+        return yScale(d.second_filter);
       })
     .attr("r",
       function(d) {
@@ -98,21 +113,21 @@ function render(dataS, data1, data2) {
 // Last Updated: 12/22/2015
 
 // Import Data
-d3.csv("./raw_data/Popular_indicators_Data.csv", type, function(data) {
+d3.csv("./raw_data/Popular_indicators_Data0.csv", type, function(data) {
   data1 = [];
   data2 = [];
   for (var i = 0; i < data.length; i++) {
     if (filter1 === data[i]["Series Name"]) {
       data1.push(data[i]);
-    } else if (filter2 === data[i]["Series Name"]){
+    } else if (filter2 === data[i]["Series Name"]) {
       data2.push(data[i]);
-    }else {}
+    } else {}
   }
   // console.log(data1);
   // console.log(data2);
   dataS = [];
-  for (var j = 0; j < data1.length; j ++) {
-    data1[j]["second_filter"] = data2[j][yColumn];
+  for (var j = 0; j < data1.length; j++) {
+    data1[j].second_filter = data2[j][yColumn];
   }
   console.log(data1);
   render(dataS, data1, data2);
