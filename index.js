b97bcef86939ce14c3d15b1e668b91821eac4f65
Markham Shofner <markham.shofner@gmail.com>
2016-01-14 19:19:34 -0500

    adjust port for heroku deployment

--- index.js
+++ index.js
@@ -35,7 +35,7 @@ app.get("/data2", cors(), function (req, res) {
   });
 });
 
-app.listen(3000, function(){
+app.listen(process.env.PORT || 3000, function(){
   console.log("listening on port 3k");
 });
 
