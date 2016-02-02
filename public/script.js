d91bb3839961a1ef62aa6ee8f0207a6e1ed0640d
Markham Shofner <markham.shofner@gmail.com>
2016-02-02 15:50:38 -0500

    adjust links for deployment

--- public/script.js
+++ public/script.js
@@ -108,10 +108,10 @@ var playground = {
   },
   retrieveData: function() {
     var self = this;
-    var url1 = 'http://localhost:3000/data/' + this.filtered.filter1;
-    var url2 = 'http://localhost:3000/data/' + this.filtered.filter2;
-    // var url1 = 'https://worldbankindicators.herokuapp.com/data/' + this.filtered.filter1;
-    // var url2 = 'https://worldbankindicators.herokuapp.com/data/' + this.filtered.filter2;
+    // var url1 = 'http://localhost:3000/data/' + this.filtered.filter1;
+    // var url2 = 'http://localhost:3000/data/' + this.filtered.filter2;
+    var url1 = 'https://worldbankindicators.herokuapp.com/data/' + this.filtered.filter1;
+    var url2 = 'https://worldbankindicators.herokuapp.com/data/' + this.filtered.filter2;
 
     // make calls to retrieve world bank data
     d3.json(url1, function(error, results) {
