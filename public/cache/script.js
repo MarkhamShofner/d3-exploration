788f393dfe423c29b9747e3a8070bc94623a01ca
Markham Shofner <markham.shofner@gmail.com>
2016-02-02 10:05:04 -0500

    update directory structure and format file names for clarity

---
+++ public/cache/script.js
@@ -0,0 +1,143 @@
+
+// base size/positioning variables
+var outerWidth = 500;
+var outerHeight = 500;
+var margin = {
+  left: 90,
+  top: 30,
+  right: 90,
+  bottom: 30
+};
+var circleRadius = 2;
+
+// tethering size of the circle to another variable
+var rMin = 2;
+var rMax = 7;
+
+var innerWidth = outerWidth - margin.left - margin.right;
+var innerHeight = outerHeight - margin.top - margin.bottom;
+
+// dataset columns
+var xColumn = "2010 [YR2010]";
+var yColumn = "2011 [YR2011]";
+var rColumn = "2012 [YR2012]";
+var colorColumn = "Series Name";
+
+// filtered rows
+var filter2 = "GDP growth (annual %)";
+var filter1 = "GNI per capita, PPP (current international $)";
+var filter2 = "Foreign direct investment, net (BoP, current US$)";
+
+var filter1 = function getField() {
+  console.log("filter1");
+  return d3.select("select").node().value;
+}();
+
+d3.select("select").on("change",function(){
+  var filter1 = this.getField();
+});
+
+
+
+// create svg element based on set variable measures
+var svg = d3.select("body").append("svg")
+  .attr("width", outerWidth)
+  .attr("height", outerHeight);
+// create g element in svg for axes
+var g = svg.append("g")
+  .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+var xAxisG = g.append("g")
+  .attr("transform", "translate(0," + innerHeight + ")");
+var yAxisG = g.append("g");
+
+// set scales using size ranges for elements
+var xScale = d3.scale.linear().range([0, innerWidth]);
+var yScale = d3.scale.linear().range([innerHeight, 0]);
+var rScale = d3.scale.linear().range([rMin, rMax]);
+
+// set axes
+var xAxis = d3.svg.axis().scale(xScale).orient("bottom")
+  .ticks(5); // Use approximately 5 ticks marks.
+// .tickFormat(d3.format("s")); // Use intelligent abbreviations, e.g. 5M for 5 Million
+// .outerTickSize(0);          // Turn off the marks at the end of the axis.
+var yAxis = d3.svg.axis().scale(yScale).orient("left");
+
+//render data
+function render(dataS, data1, data2) {
+  // set domains based on input data
+  xScale.domain(d3.extent(data1, function(d) {
+    return d[xColumn];
+  }));
+  yScale.domain(d3.extent(data2, function(d) {
+    return d[yColumn];
+  }));
+  rScale.domain(d3.extent(data1, function(d) {
+    return d[rColumn];
+  }));
+  var colorScale = d3.scale.category20();
+
+  // apply x/yAxis functionality to respective x/yAxisG group d3 element
+  xAxisG.call(xAxis);
+  yAxisG.call(yAxis);
+
+  // var data = [data1, data2];
+
+  // Bind data
+  var circles = g.selectAll("circle").data(data1);
+  // Enter
+  circles.enter().append("circle");
+
+  // Update
+  circles
+    .attr("cx", function(d) {
+      return xScale(d[xColumn]);
+    })
+    .attr("cy",
+      function(d) {
+        return yScale(d.second_filter);
+      })
+    .attr("r",
+      function(d) {
+        return rScale(d[rColumn]);
+      })
+    .attr("fill",
+      function(d) {
+        return colorScale(d[colorColumn]);
+      });
+
+  // Exit
+  circles.exit().remove();
+}
+
+
+// Data from database: World Development Indicators
+// Last Updated: 12/22/2015
+
+// Import Data
+d3.csv("./raw_data/Popular_indicators_Data0.csv", type, function(data) {
+  data1 = [];
+  data2 = [];
+  for (var i = 0; i < data.length; i++) {
+    if (filter1 === data[i]["Series Name"]) {
+      data1.push(data[i]);
+    } else if (filter2 === data[i]["Series Name"]) {
+      data2.push(data[i]);
+    } else {}
+  }
+  // console.log(data1);
+  // console.log(data2);
+  dataS = [];
+  for (var j = 0; j < data1.length; j++) {
+    data1[j].second_filter = data2[j][yColumn];
+  }
+  console.log(data1);
+  render(dataS, data1, data2);
+});
+
+// set data #s/strings to floats
+function type(d) {
+  d[xColumn] = parseFloat(d[xColumn]);
+  d[yColumn] = parseFloat(d[yColumn]);
+  d[rColumn] = parseFloat(d[rColumn]);
+  return d;
+}
--- public/script.js
+++ public/script.js
@@ -1,143 +1,174 @@
+// set up an object that represents the future action in the svg playground
+var playground = {
+  svgAttr: {
+    outerWidth: 500, //parseFloat(d3.select('.svgContain').style('width')),
+    outerHeight: 500, //parseFloat(d3.select('.svgContain').style('height')),
+    margin: {
+      left: 90,
+      right: 30,
+      top: 90,
+      bottom: 30
+    },
+    circleRadius: 2,
+    rMin: 2,
+    rMax: 7,
+  },
+  filtered: {
+    //static choices pre-user selections
+    filter1: "NY.GDP.MKTP.KD.ZG",
+    filter2: "NY.GDP.MKTP.KD.ZG",
+  },
+  page: {
+    scales: {},
+  },
+  setPlayground: function() {
+    var innerWidth = this.svgAttr.outerWidth - this.svgAttr.margin.left - this.svgAttr.margin.right;
+    var innerHeight = this.svgAttr.outerHeight - this.svgAttr.margin.top - this.svgAttr.margin.bottom;
 
-// base size/positioning variables
-var outerWidth = 500;
-var outerHeight = 500;
-var margin = {
-  left: 90,
-  top: 30,
-  right: 90,
-  bottom: 30
-};
-var circleRadius = 2;
+    // create svg element based on set variable measures
+    this.page.svg = d3.select("body").append("svg")
+      .attr("width", this.svgAttr.outerWidth)
+      .attr("height", this.svgAttr.outerHeight);
+    // create g element in svg for axes
+    this.page.g = this.page.svg.append("g")
+      .attr("transform", "translate(" + this.svgAttr.margin.left + "," + this.svgAttr.margin.top + ")");
+    this.page.xAxisG = this.page.g.append("g")
+      .attr("transform", "translate(0," + innerHeight + ")");
+    this.page.yAxisG = this.page.g.append("g");
 
-// tethering size of the circle to another variable
-var rMin = 2;
-var rMax = 7;
+    // set scales using size ranges for elements
+    this.page.scales.xScale = d3.scale.linear().range([0, innerWidth]);
+    this.page.scales.yScale = d3.scale.linear().range([innerHeight, 0]);
+    this.page.scales.rScale = d3.scale.linear().range([this.svgAttr.rMin, this.svgAttr.rMax]);
 
-var innerWidth = outerWidth - margin.left - margin.right;
-var innerHeight = outerHeight - margin.top - margin.bottom;
+    // set axes
+    this.page.xAxis = d3.svg.axis().scale(this.page.scales.xScale).orient("bottom")
+      .ticks(5); // Use approximately 5 ticks marks.
+    // .tickFormat(d3.format("s")); // Use intelligent abbreviations, e.g. 5M for 5 Million
+    // .outerTickSize(0);          // Turn off the marks at the end of the axis.
+    this.page.yAxis = d3.svg.axis().scale(this.page.scales.yScale).orient("left");
 
-// dataset columns
-var xColumn = "2010 [YR2010]";
-var yColumn = "2011 [YR2011]";
-var rColumn = "2012 [YR2012]";
-var colorColumn = "Series Name";
+  },
+  render: function(data1) {
+    var self = this;
+    // set domains based on input data
+    self.page.scales.xScale.domain(d3.extent(data1, function(d) {
+      return d.value;
+    }));
 
-// filtered rows
-var filter2 = "GDP growth (annual %)";
-var filter1 = "GNI per capita, PPP (current international $)";
-var filter2 = "Foreign direct investment, net (BoP, current US$)";
+    self.page.scales.yScale.domain(d3.extent(data1, function(d) {
+      return d.value2;
+    }));
 
-var filter1 = function getField() {
-  console.log("filter1");
-  return d3.select("select").node().value;
-}();
+    // self.page.scales.rScale.domain(d3.extent(data1, function(d) {
+    //   return d.value;
+    // }));
+    // self.page.scales.colorScale = d3.scale.category20();
 
-d3.select("select").on("change",function(){
-  var filter1 = this.getField();
-});
+    // apply x/yAxis functionality to respective x/yAxisG group d3 element
+    self.page.xAxisG.call(self.page.xAxis);
+    self.page.yAxisG.call(self.page.yAxis);
 
+    // Bind data
+    circles = self.page.g.selectAll("circle").data(data1);
+    // Enter
+    circles.enter().append("circle");
 
-
-// create svg element based on set variable measures
-var svg = d3.select("body").append("svg")
-  .attr("width", outerWidth)
-  .attr("height", outerHeight);
-// create g element in svg for axes
-var g = svg.append("g")
-  .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-var xAxisG = g.append("g")
-  .attr("transform", "translate(0," + innerHeight + ")");
-var yAxisG = g.append("g");
-
-// set scales using size ranges for elements
-var xScale = d3.scale.linear().range([0, innerWidth]);
-var yScale = d3.scale.linear().range([innerHeight, 0]);
-var rScale = d3.scale.linear().range([rMin, rMax]);
-
-// set axes
-var xAxis = d3.svg.axis().scale(xScale).orient("bottom")
-  .ticks(5); // Use approximately 5 ticks marks.
-// .tickFormat(d3.format("s")); // Use intelligent abbreviations, e.g. 5M for 5 Million
-// .outerTickSize(0);          // Turn off the marks at the end of the axis.
-var yAxis = d3.svg.axis().scale(yScale).orient("left");
-
-//render data
-function render(dataS, data1, data2) {
-  // set domains based on input data
-  xScale.domain(d3.extent(data1, function(d) {
-    return d[xColumn];
-  }));
-  yScale.domain(d3.extent(data2, function(d) {
-    return d[yColumn];
-  }));
-  rScale.domain(d3.extent(data1, function(d) {
-    return d[rColumn];
-  }));
-  var colorScale = d3.scale.category20();
-
-  // apply x/yAxis functionality to respective x/yAxisG group d3 element
-  xAxisG.call(xAxis);
-  yAxisG.call(yAxis);
-
-  // var data = [data1, data2];
-
-  // Bind data
-  var circles = g.selectAll("circle").data(data1);
-  // Enter
-  circles.enter().append("circle");
-
-  // Update
-  circles
-    .attr("cx", function(d) {
-      return xScale(d[xColumn]);
-    })
-    .attr("cy",
-      function(d) {
-        return yScale(d.second_filter);
+    // Update
+    circles
+      .transition().duration(1000)
+      .attr("cx", function(d) {
+        return self.page.scales.xScale(d.value);
       })
-    .attr("r",
-      function(d) {
-        return rScale(d[rColumn]);
-      })
-    .attr("fill",
-      function(d) {
-        return colorScale(d[colorColumn]);
+      .attr("cy",
+        function(d) {
+          return self.page.scales.yScale(d.value2);
+        })
+      .attr("r", 5);
+
+    circles
+      .on("mouseover", function(d){
+        console.log(d);
+        // create viz div based on d
       });
+    // function(d) {
+    //   return self.page.scales.rScale(d.value);
+    // });
+    // .attr("fill",
+    //   function(d) {
+    //     return self.page.scales.colorScale(d[self.filtered.colorColumn]);
+    //   });
 
-  // Exit
-  circles.exit().remove();
-}
+    // Exit
+    circles.exit().remove();
+  },
+  retrieveData: function() {
+    var self = this;
+    var url1 = 'http://localhost:3000/data/' + this.filtered.filter1;
+    var url2 = 'http://localhost:3000/data/' + this.filtered.filter2;
+    // var url1 = 'https://worldbankindicators.herokuapp.com/data/' + this.filtered.filter1;
+    // var url2 = 'https://worldbankindicators.herokuapp.com/data/' + this.filtered.filter2;
 
+    // make calls to retrieve world bank data
+    d3.json(url1, function(error, results) {
+      var data1 = [];
+      var data2 = [];
+      data1 = results[1];
+      console.log(data1);
+      // augment the first calls data using data from the second call
+      $.getJSON(url2, function(data) {
+        data2 = data[1];
+        dataC = [];
+        for (var i = 0; i < data1.length; i++) {
+          data1[i].value2 = data2[i].value;
+          data1[i].value = parseFloat(data1[i].value);
+          data1[i].value2 = parseFloat(data1[i].value2);
+          if (!isNaN(data1[i].value) && !isNaN(data1[i].value2)) {
+            console.log("true values");
+            dataC.push(data1[i]);
+            // console.log(typeof data1[i].value);
+            // console.log(dataC);
+          }
+        }
+        // console.log(dataC.length);
+        // console.log(dataC);
+        console.log("break");
+        // render the full data set
+        // console.log(data1);
+        // console.log(dataC);
+        self.render(dataC);
+      });
+    });
 
-// Data from database: World Development Indicators
-// Last Updated: 12/22/2015
-
-// Import Data
-d3.csv("./raw_data/Popular_indicators_Data0.csv", type, function(data) {
-  data1 = [];
-  data2 = [];
-  for (var i = 0; i < data.length; i++) {
-    if (filter1 === data[i]["Series Name"]) {
-      data1.push(data[i]);
-    } else if (filter2 === data[i]["Series Name"]) {
-      data2.push(data[i]);
-    } else {}
+  },
+  getFirstField: function() {
+    return d3.select("#firstFilter").node().value;
+  },
+  getSecondField: function() {
+    return d3.select("#secondFilter").node().value;
+  },
+  changeListen1: function() {
+    var self = this;
+    d3.select("#firstFilter").on("change", function() {
+      self.filtered.filter1 = self.getFirstField();
+      self.retrieveData();
+    });
+  },
+  changeListen2: function() {
+    var self = this;
+    d3.select("#secondFilter").on("change", function() {
+      self.filtered.filter2 = self.getSecondField();
+      self.retrieveData();
+    });
+  },
+  initialize: function() {
+    this.setPlayground();
+    this.retrieveData();
+    this.changeListen1();
+    this.changeListen2();
   }
-  // console.log(data1);
-  // console.log(data2);
-  dataS = [];
-  for (var j = 0; j < data1.length; j++) {
-    data1[j].second_filter = data2[j][yColumn];
-  }
-  console.log(data1);
-  render(dataS, data1, data2);
+};
+
+$(document).ready(function() {
+  playground.initialize();
 });
-
-// set data #s/strings to floats
-function type(d) {
-  d[xColumn] = parseFloat(d[xColumn]);
-  d[yColumn] = parseFloat(d[yColumn]);
-  d[rColumn] = parseFloat(d[rColumn]);
-  return d;
-}
